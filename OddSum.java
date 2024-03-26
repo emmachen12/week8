de66ddd3b7e4ac1a57213a07642382c6e36d014d
emmachen12 <emc1207pi@gmail.com>
2024-03-26 14:19:10 -0400

    g

--- OddSum.java
+++ OddSum.java
@@ -2,20 +2,20 @@ import java.util.*;
     public class OddSum{
         public static void main (String[]args)
     {
-    int num;
+    int sum;
     
 
     Scanner input = new Scanner(System.in);
     System.out.print("Enter an odd number: ");
-    num = input.nextInt();
+    int num = input.nextInt();
     input.close();
 
-    for (int num2 = 1; num2 <= num; num2 += 2)
+    for (int num2 = 1; num2 <= num; num2 = num2 + 2)
     {
-    num *= (num2-1);
+    sum += (num2);
     }
 
-    System.out.print(num);
+    System.out.print(sum);
     }
 
 }
